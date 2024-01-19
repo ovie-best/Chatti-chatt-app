c59542fb4bd738ea2721b3bbe6bb70ef4105a570
Oviedev <86495524+ovie-best@users.noreply.github.com>
2024-01-19 21:06:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      {
+        headers: { "private-key": " 65151d91-993f-4636-ae66-b68b65947d14" },
+      }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(5000);
